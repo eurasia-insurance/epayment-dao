b264ab51fb621777edbd96d4897637c5c11aad27
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-01-12 17:35:49 +0600

    removed

--- client/src/main/java/tech/lapsa/epayment/dao/EJBConstants.java
+++
@@ -1,7 +0,0 @@
-package tech.lapsa.epayment.dao;
-
-public interface EJBConstants {
-
-    public static final String APPLICATION_NAME = "epayment-dao-app";
-    public static final String MODULE_NAME = "epayment-dao";
-}
