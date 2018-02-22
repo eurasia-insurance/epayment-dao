53f10093eccb6ba7cc3389e0a93f460162b32825
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-02-22 09:39:28 +0600

    new module

---
+++ webapp/src/main/java/tech/lapsa/epayment/dao/web/ControlCDIBean.java
@@ -0,0 +1,31 @@
+package tech.lapsa.epayment.dao.web;
+
+import java.io.Serializable;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import tech.lapsa.epayment.dao.beans.EntityManagerControlBean;
+
+@Named("control")
+@RequestScoped
+@RolesAllowed("role-admin")
+public class ControlCDIBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @EJB
+    private EntityManagerControlBean bean;
+
+    public String doFlushAndClear() {
+	bean.flushAndClear();
+	return "home";
+    }
+
+    public String doPing() {
+	bean.ping();
+	return "home";
+    }
+}
